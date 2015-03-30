a2334cd46dc6119393c433857d5e6555e97e6d3e
AlexiaCoue <alexia.coue@bts-malraux.net>
2015-03-30 17:27:10 +0200

    djksqjl

--- plateau.cpp
+++ plateau.cpp
@@ -7,13 +7,13 @@ plateau::plateau() : QGraphicsScene()
 {
     QGraphicsPixmapItem * mon_item;
     QPixmap * croix = new QPixmap(":/croix.png");
-    for(int i; i < 5; i++)
+    for(int i=0; i < 5; i++)
     {
-        for(int j; j < 5 ; j++)
+        for(int j=0; j < 5 ; j++)
         {
             mon_item = new QGraphicsPixmapItem();
             mon_item->setPixmap(* croix);
-            mon_item->setPos(i*55, j*55);
+            mon_item->setPos(i*65, j*65);
             this->addItem(mon_item);
         }
     }
